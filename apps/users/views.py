8ab81fd0a742b594b9539324c7ec082c3b559ae0
Toktorov <ktoktorov2005@gmail.com>
2023-12-11 18:40:29 +0600

    add serializer and views for user

--- apps/users/views.py
+++ apps/users/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
+from rest_framework.generics import ListCreateAPIView, RetrieveAPIView, UpdateAPIView, DestroyAPIView
+
+from apps.users.models import User
+from apps.users.serializers import UserSerializer, UserRegisterSerializer
 
 # Create your views here.
+class UserListCreateAPIView(ListCreateAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+class UserRetrieveAPIView(RetrieveAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+class UserUpdateAPIView(UpdateAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+class UserDestroyAPIView(DestroyAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
