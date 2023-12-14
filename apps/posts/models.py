fed3038c65f96cf279f6391f24d8d0f9564a67e0
Toktorov <ktoktorov2005@gmail.com>
2023-12-14 19:50:51 +0600

    post user model

--- apps/posts/models.py
+++ apps/posts/models.py
@@ -1,6 +1,9 @@
 from django.db import models
+from django.contrib.auth import get_user_model
 
 from apps.categories.models import Category
+
+User = get_user_model()
 
 # Create your models here.
 class Post(models.Model):
@@ -8,6 +11,12 @@ class Post(models.Model):
         Category, on_delete=models.SET_NULL,
         related_name="category_posts",
         blank=True, null=True
+    )
+    user = models.ForeignKey(
+        User, on_delete=models.SET_NULL,
+        related_name="users_posts",
+        blank=True, null=True,
+        verbose_name = "Пользователь"
     )
     title = models.CharField(
         max_length=255,
