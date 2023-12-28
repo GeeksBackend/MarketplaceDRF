9861e3466056b9dc216ef194c3755515ced84f93
Toktorov <ktoktorov2005@gmail.com>
2023-12-28 18:48:55 +0600

    ready to prod

--- core/settings.py
+++ core/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-6ap0@*#tjbql1&^bh8v%f31jd6u$p_!0-3s@c(2fn#@h3u@)u@
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
